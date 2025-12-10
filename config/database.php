20e85eb75fc76c498bb988029e0e761361b276fc
Gowtham V <gowthamdvr@gmail.com>
2025-12-10 10:22:09 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,24 +2,32 @@
 // config/database.php
 
 class Database {
-   private $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+    private $host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
     private $port = 4000;
     private $db_name = "test";
-    private $username = "wrvr86JNUurTeWf.root";
-    private $password = "qKK70gh40nF28kL0";  // <-- Change immediately
-    private $ca = "C:\Users\LAB8\Downloads";
+    private $username = "FSQsQmvtgfFX5Nt.root";
+    private $password = "Vn3D4RqPmQ6Tx3Vc";
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
+            $options = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_SSL_CA => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                PDO::ATTR_TIMEOUT => 10,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_STRINGIFY_FETCHES => false
             );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conn->exec("set names utf8");
+            
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name . ";charset=utf8mb4",
+                $this->username,
+                $this->password,
+                $options
+            );
+            $this->conn->exec("set names utf8mb4");
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
@@ -28,6 +36,3 @@ class Database {
 }
 
 ?>
-
-
-
